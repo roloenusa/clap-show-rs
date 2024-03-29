e300505840235875d5c88ed027312762f59356cf
roloenusa <roloenusa@msn.com>
2024-03-28 23:14:51 -0700

    Working with handle bars

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Generate documentation for clap command-line tools
 
-static TEMPLATE_FILE: &'static [u8] = include_bytes!("./test.html");
+// static TEMPLATE_FILE: &'static [u8] = include_bytes!("./test.html");
 
 #[allow(dead_code)]
 // Ensure that doc tests in the README.md file get run.
@@ -9,30 +9,40 @@ mod test_readme {
     #![doc = include_str!("../README.md")]
 }
 
-use std::fmt::Write;
+use std::fmt::{Arguments, Write};
 
 use clap::{Arg, Command};
-use ramhorns::{Content, Template};
+use handlebars::Handlebars;
+use serde_derive::Serialize;
 
-#[derive(Content)]
-struct FmtArg<'a> {
+#[derive(Serialize, Clone, Debug)]
+struct FmtArg {
     flags: String,
-    description: &'a str
+    description: String
 }
 
-#[derive(Content)]
-struct FmtCmd<'a> {
-    flags: String,
-    description: &'a str
-}
-
-
-#[derive(Content)]
-struct FmtCommands<'a> {
-    title: String,
+#[derive(Serialize, Clone, Debug)]
+struct FmtCmd {
+    name: String,
     description: String,
-    commands: Vec<FmtCmd<'a>>,
-    options: Vec<FmtArg<'a>>,
+}
+
+
+#[derive(Serialize, Clone, Debug)]
+struct FmtCommands {
+    title: String,
+    usage: String,
+    cmd_chain: String,
+    description: String,
+    commands: Vec<FmtCmd>,
+    arguments: Vec<FmtArg>,
+    options: Vec<FmtArg>,
+}
+
+#[derive(Serialize, Clone, Debug)]
+struct Page {
+    main: FmtCommands,
+    subcommands: Vec<FmtCommands>
 }
 
 /// Format the help information for `command` as Markdown.
@@ -85,253 +95,159 @@ fn write_help(buffer: &mut String, command: &clap::Command) {
     .unwrap();
 
     build_cmd(&command);
-
-    // let host_str = std::str::from_utf8(TEMPLATE_FILE).unwrap();
-    // let tpl = Template::new(host_str).unwrap();
-    // let rendered = tpl.render(&FmtCommands {
-    //     title: title_name,
-    //     description: buffer,
-    //     options
-    // });
 }
 
-struct HlpCmd {
-    name: String,
-    long_flag: String,
-    short_flag: String,
-
-    // long_about: String,
-    short_about: String,
-
-    length: usize,
-    options: Vec<HlpArg>,
-    commands: Vec<HlpCmd>,
+trait Cmd {
+    fn fmt_cmd(&self, parent: Vec<String>) -> FmtCommands;
+    fn get_usage(&mut self) -> String;
 }
 
-impl HlpCmd {
-    fn new(cmd: &Command) -> Self {
-        let short_about = match cmd.get_about() {
+impl Cmd for Command {
+    fn get_usage(&mut self) -> String {
+        let parts = self.render_usage().to_string();
+        let parts = parts.split(" ").collect::<Vec<&str>>();
+        let slice = parts[2..].iter().map(|v| v.to_string()).collect::<Vec<String>>();
+        slice.join(" ").to_string()
+    }
+
+    fn fmt_cmd(&self, parents: Vec<String>) -> FmtCommands {
+
+        // TODO: Get the actual long about
+        let long_about = match self.get_about() {
             Some(value) => value.to_string(),
             None => String::new(),
         };
 
-        let long_flag = match cmd.get_long_flag() {
-            Some(value) => format!("--{}", value),
-            None => String::new(),
-        };
-
-        let short_flag = match cmd.get_short_flag() {
-            Some(value) => format!("-{}", value),
-            None => String::new(),
-        };
-
-        HlpCmd {
-            name: cmd.get_name().to_string(),
-            long_flag,
-            short_flag,
-            // long_about,
-            short_about,
-            length: 0,
-            options: Vec::new(),
-            commands: Vec::new(),
-        }
-    }
-
-    fn max_len(&mut self, i: usize) -> &Self {
-        if i > self.length {
-            self.length = i;
-        }
-        self
-    }
-
-    fn add_arg(&mut self, arg: HlpArg) -> &Self {
-        let size = &arg.long.len();
-        self.options.push(arg);
-        self.max_len(*size);
-        self
-    }
-
-    fn add_cmd(&mut self, cmd: HlpCmd) -> &Self {
-        self.commands.push(cmd);
-        self
-    }
-
-    fn print(&self) {
-        let title = self.name.to_owned();
-
-        self.print_commands();
-
+        let mut arguments: Vec<FmtArg> = Vec::new();
         let mut options: Vec<FmtArg> = Vec::new();
-        if !self.options.is_empty() {
-            for arg in self.options.iter() {
+        for arg in self.get_arguments() {
 
+            if arg.is_positional() {
+                arguments.push(FmtArg {
+                    flags: arg.fmt_flags(),
+                    description: match arg.get_long_help() {
+                        Some(value) => value.to_string(),
+                        None => String::new(),
+                    }
+                });
+            } else {
                 options.push(FmtArg {
                     flags: arg.fmt_flags(),
-                    description: &arg.description,
+                    description: match arg.get_long_help() {
+                        Some(value) => value.to_string(),
+                        None => String::new(),
+                    }
                 });
             }
-
-
         }
 
-        let host_str = std::str::from_utf8(TEMPLATE_FILE).unwrap();
-        let tpl = Template::new(host_str).unwrap();
-        let rendered = tpl.render(&FmtCommands {
-            title,
-            description: self.short_about.to_owned(),
-            commands: Vec::new(),
-            options
-        });
-        println!("{}", rendered);
-    }
-
-    fn calculate_width(&self) -> usize {
-        let mut width: usize = 0;
-        for cmd in self.commands.iter() {
-            let name_length = match cmd.name.len() {
-                0 => 0,
-                value => value + 2, // value + comma + space
-            };
-
-            let short_length = match cmd.short_flag.len() {
-                0 => 0,
-                value => value + 2, // value + single-dash + comma + space
-            };
-
-            let long_length = match cmd.long_flag.len() {
-                0 => 0,
-                value => value + 1, // value + double-dash + comma
-            };
-
-            width = std::cmp::max(width, name_length + short_length + long_length);
-        }
-        width += 2; // 2 trailing spaces
-
-        return width;
-    }
-
-    fn print_commands(&self) -> String {
-        if self.commands.is_empty() {
-            return String::new();
+        // Format the subcommands
+        let mut subcommands: Vec<FmtCmd> = Vec::new();
+        for command in self.get_subcommands() {
+            subcommands.push(FmtCmd {
+                name: command.get_name().to_string(),
+                description: match command.get_after_long_help() {
+                    Some(t) => t.to_string(),
+                    _ => String::new(),
+                }
+            });
         }
 
-        let width = self.calculate_width();
+        let mut cmd = self.clone();
+        let usage = cmd.get_usage();
 
-        println!("Commands");
+        let mut ancestors = parents.clone();
+        ancestors.push(self.get_name().to_string());
 
-        let mut f = String::new();
-        for cmd in self.commands.iter() {
-            let name = match &cmd.name.is_empty() {
-                false => format!("{}, ", cmd.name),
-                true => "".to_string(),
-            };
-
-            let short_flag = match &cmd.short_flag.is_empty() {
-                false => format!("{}, ", cmd.short_flag),
-                true => "".to_string(),
-            };
-
-            let long_flag = match &cmd.long_flag.is_empty() {
-                false => format!("{}", cmd.long_flag),
-                true => "".to_string(),
-            };
-
-            writeln!(
-                f,
-                "{:width$} {}",
-                format!("  {}{}{}", name, short_flag, long_flag),
-                &cmd.short_about
-            )
-            .unwrap();
+        FmtCommands {
+            title: self.get_name().to_string(),
+            usage,
+            cmd_chain: ancestors.join(" "),
+            description: long_about,
+            commands: subcommands,
+            arguments,
+            options,
         }
-
-        f
     }
 }
 
-#[derive(Clone)]
-struct HlpArg {
-    short: String,
-    long: String,
-    values: Vec<String>,
-    description: String,
 
+
+/**
+ * FLAG BLOCK
+ */
+trait Flag {
+    fn fmt_flags(&self) -> String;
 }
 
-impl HlpArg {
-    fn new(arg: &Arg) -> Self {
-        let short = match arg.get_short() {
+impl Flag for Arg {
+    fn fmt_flags(&self) -> String {
+        let short = match self.get_short() {
             Some(value) => format!("-{}", value),
             None => String::new(),
         };
-        let long = match arg.get_long() {
+        let long = match self.get_long() {
             Some(value) => format!("--{}", value),
             None => String::new(),
         };
-        let values = match arg.get_action().takes_values() {
-            true => match arg.get_value_names() {
+        let values = match self.get_action().takes_values() {
+            true => match self.get_value_names() {
                 // TODO: What if multiple names are provided?
                 Some([]) => Vec::new(),
                 Some(value) => value.iter().map(|f| format!("<{}>", f)).collect::<Vec::<String>>(),
-                None => vec![format!("<{}>", arg.get_id())],
+                None => vec![format!("<{}>", self.get_id())],
             },
             false => Vec::new(),
         };
 
-        let description = match arg.get_help() {
-            Some(value) => value.to_string(),
-            None => String::new(),
-        };
-
-        HlpArg {
-            short,
-            long,
-            values,
-            description,
-        }
-    }
-
-    fn fmt_flags(&self) -> String {
         // print short arg, and add a comma if a long arg exists
-        let mut s = format!("{:min$}", self.short, min = 2);
+        let mut s = format!("{:min$}", short, min = 2);
 
-        if self.long.len() > 0 {
+        if long.len() > 0 {
             // Add a comma if there is a long arg, otherwise just a space
-            if self.short.len() > 0 {
+            if short.len() > 0 {
                 s.push_str(", ");
             } else {
                 s.push_str("  ");
             }
-            s.push_str(format!("{}", self.long).as_str());
+            s.push_str(format!("{}", long).as_str());
         };
 
-        if self.values.len() > 0 {
-            s.push_str(format!(" {}", self.values.join(" ")).as_str());
+        if values.len() > 0 {
+            s.push_str(format!(" {}", values.join(" ")).as_str());
         }
 
         return s;
     }
 }
 
-fn build_cmd(command: &Command) -> HlpCmd {
-    let mut hc = HlpCmd::new(&command);
 
-    for pos_arg in command.get_positionals() {
-        let short = pos_arg.get_long();
-        let long = pos_arg.get_long();
-        let desc = pos_arg.get_help();
-        println!("-{:#?}\t --{:#?}\t Short: {:#?}", short, long, desc);
-    }
+fn build_cmd(command: &Command) -> &Command {
+    let hc = &command;
+    let fmt_command = hc.fmt_cmd(Vec::new());
 
-    for cmd in command.get_subcommands() {
-        hc.add_cmd(HlpCmd::new(&cmd));
-    }
+    let mut children_commands: Vec<FmtCommands> = Vec::new();
+    let parents: Vec<String> = Vec::new();
+    extract_subcommands(command, &mut children_commands, parents);
 
-    for pos_arg in command.get_arguments() {
-        hc.add_arg(HlpArg::new(&pos_arg));
-    }
+    let mut handlebars = Handlebars::new();
+    handlebars.register_template_file("template", "/Users/roloenusa/projects/personal/clap-show-rs/src/test.html").unwrap();
+    // handlebars.register_template_file("test_p", "test_p.html").unwrap();
 
-    hc.print();
+
+    println!("{}", handlebars.render("template", &Page{ main: fmt_command, subcommands: children_commands }).unwrap());
 
     hc
+}
+
+fn extract_subcommands(subcommand: &Command, children_commands: &mut Vec<FmtCommands>, parents: Vec<String>) {
+    let mut parents: Vec<String> = parents;
+    parents.push(subcommand.get_name().to_string());
+
+    for subcommand in subcommand.get_subcommands() {
+        let subcommand = subcommand.to_owned();
+        children_commands.push(subcommand.fmt_cmd(parents.clone()));
+
+        extract_subcommands(&subcommand, children_commands, parents.clone());
+    }
 }
